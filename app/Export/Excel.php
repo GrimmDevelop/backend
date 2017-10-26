6791310155504ce347417217112c5c11c058a7e5
Ibrahim Hilali <ebrahelaly@gmail.com>
2017-10-26 23:21:55 +0200

    Added changes to Excel class
    -changed $data type to Collection
    -changed save methode to be able to save data in the save file in the same sheet or another
    -edited title method to be able to set title to an exists sheet with out create one

--- app/Export/Excel.php
+++ app/Export/Excel.php
@@ -2,14 +2,20 @@
 
 namespace App\Export;
 
+use function array_merge;
+use function collect;
+use function file_exists;
+use Illuminate\Support\Collection;
+use function in_array;
 use PHPExcel_IOFactory;
 use PHPExcel;
 
-class Excel
+class Excel implements Export
 {
 
     private $excel = null;
     private $writer = null;
+    private $reader = null;
     private $config;
 
     /**
@@ -26,42 +32,82 @@ class Excel
         $this->writer = PHPExcel_IOFactory::createWriter($this->excel, $this->config['type']);
     }
 
+    /**
+     * @param string $title
+     * @param int $sheet
+     * @return $this
+     */
     public function title($title, $sheet = 0)
     {
         // create and set title for excel sheet
+        //and if there is sheet with this title rename it
 
-        $this->excel->createSheet($sheet);
+        if (not(in_array($title, $this->excel->getSheetNames()))) {
+            $this->excel->createSheet($sheet);
+        }
         $this->excel->setActiveSheetIndex($sheet)->setTitle($title);
         return $this;
     }
 
     /**
-     * @param array $data
+     * @param Collection $data
      * @param $intoSheet
      * @param bool $firstRowAreColumnNames
      * @return $this
      */
-    public function load(array $data, $intoSheet = null, $firstRowAreColumnNames = false)
+    public function load(Collection $data, $intoSheet = null, $firstRowAreColumnNames = false)
     {
         // load to excel sheet
 
         $intoSheet = $intoSheet == null ? $this->excel->getActiveSheetIndex() : $intoSheet;
-        $this->excel->setActiveSheetIndex($intoSheet)->fromArray($data);
+        if ($firstRowAreColumnNames) {
+            $keys = collect($data[0])->keys();
+            $this->excel->setActiveSheetIndex($intoSheet)->fromArray(array_merge([$keys->toArray()], $data->toArray()));
+        } else {
+            $this->excel->setActiveSheetIndex($intoSheet)->fromArray($data->toArray());
+        }
         return $this;
     }
 
     /**
      * @param $fileName
+     * @param bool $overwriteExistingFile
      * @return bool
      */
-    public function save($fileName)
+    public function save($fileName, $overwriteExistingFile = false)
     {
         // save excel file to config('export.path')
         // return true if success otherwise false
+        // when  $overwriteExistingFile = true
+        //we write over  file and over  sheet if exists
+
+        //remove the default empty sheet
+        $this->excel->removeSheetByIndex($this->excel->getSheetCount() - 1);
+
+        if (file_exists($this->config['path'] . $fileName . '.xlsx') && $overwriteExistingFile) {
+
+            $this->reader = PHPExcel_IOFactory::createReaderForFile($this->config['path'] . $fileName . '.xlsx');
+            $oldSheets = $this->reader->load($this->config['path'] . $fileName . '.xlsx');
+            //add the  old sheets to new excel file
+            foreach ($oldSheets->getAllSheets() as $sheet) {
+                if (in_array($sheet->getTitle(), $this->excel->getSheetNames())) {
+
+                    $mergeData = array_merge($sheet->toArray(),
+                        $this->excel->getSheetByName($sheet->getTitle())->toArray());
+
+                    $this->excel->setActiveSheetIndexByName($sheet->getTitle())
+                        ->fromArray($mergeData);
+                } else {
+
+                    $this->excel->addSheet($sheet, $this->excel->getSheetCount() + 1);
+
+                }
+            }
+        }
 
         $this->writer->save($this->config['path'] . $fileName . '.xlsx');
         return true;
 
     }
-    
+
 }
