c16f34b96325bd51e3c951e0e29cb6fd8afe02bf
Freeeed <friedrich@ueberreiter.com>
2018-03-30 11:14:54 +0200

    updated PHPUnit from 6.5 to 7.0

--- tests/Unit/PersonTest.php
+++ tests/Unit/PersonTest.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace Tests\Unit;
+
 use Grimm\Person;
 use Illuminate\Support\Str;
+use Tests\TestCase;
 
 class PersonTest extends TestCase
 {
@@ -11,7 +14,7 @@ class PersonTest extends TestCase
      */
     public function a_person_can_be_scoped_by_letters()
     {
-        foreach (Person::byLetter('a')->get() as $person) {
+        foreach (Person::byPrefix('a')->get() as $person) {
             $this->assertTrue(Str::startsWith(Str::lower($person->last_name), ['a', 'ä']));
         }
     }
