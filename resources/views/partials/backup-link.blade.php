bf064de0db5c8df2a484c9549389f4257cc9ce27
Friedrich Ueberreiter <friedrich@ueberreiter.com>
2021-12-13 16:07:39 +0100

    added alternative popover text if no backup was found

--- resources/views/partials/backup-link.blade.php
+++ resources/views/partials/backup-link.blade.php
@@ -1,6 +1,6 @@
 <a href="#" class="nav-link backup-link" data-container="body" data-toggle="popover" data-placement="bottom"
    data-title="Datensicherung"
-   data-content="Name: {{ $backup->lastBackupName() }}<br>Letzter Sicherungsversuch: {{ $backup->lastBackupAttempt('d.m.Y H:i') }}<br>Letzte erfolgreiche Sicherung: {{ $backup->lastSuccessfulBackup('d.m.Y H:i') }}">
+   data-content="{{ $backup->popoverText() }}">
     <span class="badge badge-{{ $backup->statusLabel() }}">
         <span class="fa fa-cloud"></span>
     </span>
