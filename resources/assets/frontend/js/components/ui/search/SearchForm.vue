82b48649c4f90d3aafa34e4b604a6f2b617b9fb5
mayermad <mayerjunior@gmx.de>
2022-01-21 21:28:26 +0100

    added navbar to search form

--- resources/assets/frontend/js/components/ui/search/SearchForm.vue
+++ resources/assets/frontend/js/components/ui/search/SearchForm.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="complete-container">
+        <nav class="navbar navbar-light navbar-expand-lg p-4 bg-white border-b-2 border-gray-300">
+            <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                <div class="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+                    <a :href="homeURL" class="text-lg text-gray-600 mr-4 hover:text-gray-700">
+                        Grimmbriefwechsel
+                    </a>
+                    <a :href="adminUrl" class="text-base text-gray-500 mx-4 hover:text-gray-600">Verwaltung</a>
+                    <a :href="letterSearch" class="text-base text-gray-500 mx-4 hover:text-gray-600">Briefsuche</a>
+                </div>
+            </div>
+        </nav>
         <div class="card m-auto">
             <div class="card-header text-xl flex justify-between">
                 <a>Briefsuche</a>
